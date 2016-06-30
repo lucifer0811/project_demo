3e97c8dcc2a2b8ff923a8fac67b5b3b806b50158
Phan Nguyen <phannh.uetk58@gmail.com>
2016-06-30 17:43:30 +0700

    read comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+	def new
+		@entry = Entry.find params[:entry_id]
+		@comment = Comment.new
+	end
+
+	def create
+		@entry = Entry.find params[:entry_id]
+		@comment = Comment.new(comment_params)
+	end
+
+	private 
+
+	  def comment_params
+	  	params.require(:comment).permit(:comment, :params, :entry_id)
+	  end
 end
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,9 +2,13 @@ class EntriesController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user, only: :destroy
 
+  def index
+    @entries = Entry.paginate(page: params[:page])
+  end
+
   def show
     @entry = Entry.find(params[:id])
-    @comments = @entry.comments
+    @comments = @entry.comments.paginate(page: params[:page])
   end
 
   def create
